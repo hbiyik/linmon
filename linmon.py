04ec7dda06406d65d846c95fe8ecf1ff28b80305
boogie <boogie@gmx.com>
2020-02-05 23:34:51 +0100

    make sensor attributes generic

--- linmon.py
+++ linmon.py
@@ -31,6 +31,19 @@ inputs = {
     "pwm": {"unit": "%", "factor": 2.55},
 }
 
+attrs = {
+    "min": "float",
+    "max": "float",
+    "crit": "float",
+    "crit_alarm": "float",
+    "beep": "float",
+    "alarm": "float",
+    "label": "str"
+    }
+
+sens_re = ")|(?:".join(inputs.keys())
+sens_re = "((?:%s))([0-9]+)_input$" % sens_re
+
 sensors = {}
 
 
@@ -47,50 +60,43 @@ def parsefile(fpath):
             pass
 
 
-def addsensor(source, *sensor):
+def addsensor(source, sens_type, sens_num, sens_val, sens_attrs):
     if source not in sensors:
         sensors[source] = []
-    sensors[source].append(sensor)
+    sensors[source].append((sens_type, sens_num, sens_val, sens_attrs))
 
 
-def attribute(source, sensor_type, sensor_num, tag):
-    tagpath = os.path.join(source, sensor_type + sensor_num + "_" + tag)
-    return parsefile(tagpath)
+def attributes(file_path, sensor_type, sensor_num):
+    sens_attrs = {}
+    for sens_attr in attrs:
+        attr_path = os.path.join(file_path, sensor_type + sensor_num + "_" + sens_attr)
+        attr_val = parsefile(attr_path)
+        if attr_val is not None:
+            sens_attrs[sens_attr] = attr_val
+    return sens_attrs
 
 
-def readsensor(filepath, sensor):
-    sens_type = sensor.group(1)
-    sens_num = sensor.group(2)
+def readsensor(filepath, sens_type, sens_num, suffix="_input"):
+    sensfile = os.path.join(filepath, sens_type + sens_num + suffix)
     if sens_type in inputs:
-        sens_val = parsefile(filepath)
+        sens_val = parsefile(sensfile)
         if sens_val is not None:
-            addsensor(filepath, sens_type, sens_num, sens_val,
-                      attribute(filepath, sens_type, sens_num, "min"),
-                      attribute(filepath, sens_type, sens_num, "max"),
-                      attribute(filepath, sens_type, sens_num, "alarm"),
-                      attribute(filepath, sens_type, sens_num, "beep"),
-                      attribute(filepath, sens_type, sens_num, "label"))
+            sens_attrs = attributes(filepath, sens_type, sens_num)
+            addsensor(filepath, sens_type, sens_num, sens_val, sens_attrs)
 
 
 def collect():
     for subdir, _, files in os.walk("/sys"):
         for fname in files:
-            sensor = re.search("((?:curr)|(?:in)|(?:fan)|(?:temp)|(?:power))([0-9]+)_input$", fname)
+            sensor = re.search(sens_re, fname)
+            suffix = "_input"
             if not sensor:
                 sensor = re.search("((?:pwm))([0-9]+)$", fname)
+                suffix = ""
             if sensor:
-                filepath = os.path.join(subdir, fname)
                 sens_type = sensor.group(1)
                 sens_num = sensor.group(2)
-                if sens_type in inputs:
-                    sens_val = parsefile(filepath)
-                    if sens_val is not None:
-                        addsensor(subdir, sens_type, sens_num, sens_val,
-                                  attribute(subdir, sens_type, sens_num, "min"),
-                                  attribute(subdir, sens_type, sens_num, "max"),
-                                  attribute(subdir, sens_type, sens_num, "alarm"),
-                                  attribute(subdir, sens_type, sens_num, "beep"),
-                                  attribute(subdir, sens_type, sens_num, "label"))
+                readsensor(subdir, sens_type, sens_num, suffix)
 
 
 def report():
@@ -100,26 +106,26 @@ def report():
             print("%s (%s)" % (source_name, source))
         else:
             print("%s:" % source)
-        for stype, snum, sval, smin, smax, salarm, sbeep, slabel in sorted(sensors[source], key=itemgetter(0, 1)):
+        for stype, snum, sval, sattrs in sorted(sensors[source], key=itemgetter(0, 1)):
             scfg = inputs[stype]
             sname = "%s%s" % (stype, snum)
-            if slabel:
-                sname = "%s (%s)" % (sname, slabel)
             if stype == "pwm":
-                sval = "%.3f%s (%i) " % (sval / scfg["factor"], scfg["unit"], sval)
+                sval = "%.2f%s (%i) " % (sval / scfg["factor"], scfg["unit"], sval)
             else:
-                sval = "%.3f%s" % (sval / scfg["factor"], scfg["unit"])
-            sattrs = []
-            if smin:
-                sattrs.append("MIN: %.3f%s" % (smin / scfg["factor"], scfg["unit"]))
-            if smax:
-                sattrs.append("MAX: %.3f%s" % (smax / scfg["factor"], scfg["unit"]))
-            if salarm:
-                sattrs.append("ALARM: %.3f%s" % (salarm / scfg["factor"], scfg["unit"]))
-            if sbeep:
-                sattrs.append("BEEP: %.3f%s" % (sbeep / scfg["factor"], scfg["unit"]))
-            if len(sattrs):
-                sval += " (%s)" % ", ".join(sattrs)
+                sval = "%.2f%s" % (sval / scfg["factor"], scfg["unit"])
+            sattr_txts = []
+            for sattr in sattrs:
+                sattr_val = sattrs[sattr]
+                if sattr == "label":
+                    sname = "%s (%s)" % (sname, sattr_val)
+                    continue
+                elif attrs[sattr] == "float":
+                    sattr_val = "%.2f" % (sattr_val / scfg["factor"])
+                sattr_txts.append("%s: %s%s" % (sattr,
+                                                sattr_val,
+                                                scfg["unit"]))
+            if len(sattr_txts):
+                sval += " (%s)" % ", ".join(sattr_txts)
             print("    %s: %s" % (sname, sval))
 
 
