14b1aec1e46b2ddab3f90d5123c2c5c6ba15f134
boogie <boogiepop@gmx.com>
2024-11-11 23:16:44 +0100

    register new collectors

--- liblinmon/collectors/__init__.py
+++ liblinmon/collectors/__init__.py
@@ -4,6 +4,9 @@ from . import rk_saradc
 from . import devfreq
 from . import clk
 from . import cpufreq
+from . import drm
+from . import dmi
+from . import wireless
 
 registry = {
     "hwmon": hwmon.Collections(),
@@ -11,4 +14,7 @@ registry = {
     "rksaradc": rk_saradc.Collections(),
     "devfreq": devfreq.Collections(),
     "cpufreq": cpufreq.Collections(),
+    "drm": drm.Collections(),
+    "dmi": dmi.Collections(),
+    "wireless": wireless.Collections(),
     }
